b213491514bbf4506fb8fad5abf0a9e31fec3997
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-18 16:18:12 -0500

    Don't use the movement type keywords directly
    Provide a layer of indirection by making vars in the movement ns

--- src/diplomacy/game/movement.clj
+++ src/diplomacy/game/movement.clj
@@ -1,5 +1,9 @@
 (ns diplomacy.game.movement)
 
+(def coastal ::coastal)
+(def water   ::water)
+(def inland  ::inland)
+
 (defn check-province-movement-type [province expected]
   (let [movement-type (cond
                        ;; Must be iterating through a seq of provinces
@@ -16,12 +20,12 @@
 
 (defn land-locked? [province]
   (check-province-movement-type province
-                                ::inland))
+                                inland))
 
 (defn coastal? [province]
   (check-province-movement-type province
-                                ::coastal))
+                                coastal))
 
 (defn water? [province]
   (check-province-movement-type province
-                                ::water))
+                                water))
