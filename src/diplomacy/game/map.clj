2780374684a9e978c1eeae1f7776d8e8db1b8946
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-25 03:43:48 -0500

    Clean up an unused function

--- src/diplomacy/game/map.clj
+++ src/diplomacy/game/map.clj
@@ -176,13 +176,6 @@
        ~@forms
        (finally (~close-fn ~binding)))))
 
-(defn highlight-points [img color points]
-  (with-cleanup [gfx (.createGraphics img)] .dispose
-    (.setBackground gfx color)
-    (dorun
-     (map (fn [[x y]]
-            (.clearRect gfx x y 1 1))
-          points))))
 
 ;; Connected components
 
