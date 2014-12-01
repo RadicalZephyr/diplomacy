f0196720e6714add99db1b3c6a60f63cc970264f
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-01 13:30:52 -0500

    Add documentation for the inner and outer point functions

--- src/diplomacy/game/map.clj
+++ src/diplomacy/game/map.clj
@@ -102,7 +102,16 @@
                           -1 -1 -1
                           -1 -1 -16777216] :bri})
 
-(defn corner->inner-point [{:keys [type point]}]
+(defn corner->inner-point
+  "The inner point is defined as the point that is white-pixeled and
+  most in the corner.
+
+  That is for this pattern:
+  x x
+  x o
+
+  where x's are black and o's are white, the \"o\" is the \"inner point\"."
+  [{:keys [type point]}]
   (apply
    (case type
      :tlo (fn [x y] [(inc x) (inc y)])
@@ -115,7 +124,16 @@
      :bri (fn [x y] [(dec x) (dec y)]))
    point))
 
-(defn corner->outer-point [{:keys [type point]}]
+(defn corner->outer-point
+  "The outer point is definied as the point that is black-pixeled and
+  in the actual corner.
+
+  That is, for this pattern:
+  X x
+  x o
+
+  Where x's are black and o's are white, the \"X\" is the \"outer point\"."
+  [{:keys [type point]}]
   (apply
    (case type
      :tlo (fn [x y] [x y])
