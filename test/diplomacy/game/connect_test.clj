d5bb7cdf7de23dff003df1c3aac9567538f74a37
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 02:21:46 -0500

    Make tests easily changeable for multiple implementations

--- test/diplomacy/game/connect_test.clj
+++ test/diplomacy/game/connect_test.clj
@@ -4,29 +4,30 @@
 
 (deftest connected-components-test
   (testing "Recursive connected components"
-    (is (= [1 1 0 1 1 1
-            1 1 0 1 0 0
-            1 1 1 1 0 0]
-           (connected-components [-1 -1  0 -1 -1 -1
-                                  -1 -1  0 -1  0  0
-                                  -1 -1 -1 -1  0  0]
-                                 6 3
-                                 :impl :recursive)))
+    (doseq [impl [:recursive]]
+      (is (= [1 1 0 1 1 1
+              1 1 0 1 0 0
+              1 1 1 1 0 0]
+             (connected-components [-1 -1  0 -1 -1 -1
+                                    -1 -1  0 -1  0  0
+                                    -1 -1 -1 -1  0  0]
+                                   6 3
+                                   :impl :recursive)))
 
-    (is (= [1 1 0 2 2 2
-            1 1 0 2 0 0
-            1 1 1 0 0 0]
-           (connected-components [-1 -1  0 -1 -1 -1
-                                  -1 -1  0 -1  0  0
-                                  -1 -1 -1  0  0  0]
-                                 6 3
-                                 :impl :recursive)))
+      (is (= [1 1 0 2 2 2
+              1 1 0 2 0 0
+              1 1 1 0 0 0]
+             (connected-components [-1 -1  0 -1 -1 -1
+                                    -1 -1  0 -1  0  0
+                                    -1 -1 -1  0  0  0]
+                                   6 3
+                                   :impl :recursive)))
 
-    (is (= [1 1 0 2 2 2
-            1 1 0 2 0 0
-            1 1 1 0 3 0]
-           (connected-components [-1 -1  0 -1 -1 -1
-                                  -1 -1  0 -1  0  0
-                                  -1 -1 -1  0 -1  0]
-                                 6 3
-                                 :impl :recursive)))))
+      (is (= [1 1 0 2 2 2
+              1 1 0 2 0 0
+              1 1 1 0 3 0]
+             (connected-components [-1 -1  0 -1 -1 -1
+                                    -1 -1  0 -1  0  0
+                                    -1 -1 -1  0 -1  0]
+                                   6 3
+                                   :impl :recursive))))))
