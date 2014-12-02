7e10f38f4d572df42ec5c7a7a1219d5aed68a9d1
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 18:50:59 -0500

    Start adding some of the graphics utilities from the book

---
+++ src/diplomacy/graphics/utils.clj
@@ -0,0 +1,36 @@
+(ns diplomacy.graphics.utils
+  (:import java.awt.image.BufferedImage))
+
+
+(defn get-pixels [img x y w h]
+  (cond
+   (or (= w 0)
+       (= h 0)) []
+   :else
+   (let [img-type (.getType img)
+         pixels (int-array (* w h))]
+     (into []
+           (if (or (= img-type BufferedImage/TYPE_INT_ARGB)
+                   (= img-type BufferedImage/TYPE_INT_RGB))
+             (let [raster (.getRaster img)]
+               (.getDataElements raster x y w h pixels))
+             (.setRGB img x y w h pixels 0 w))))))
+
+(defn set-pixels [img x y w h pixels]
+  (cond
+   (or (not pixels)
+       (= w 0)
+       (= h 0)) nil
+   (> (* w h)
+      (count pixels))
+   (throw (ex-info "Pixels length must be greater than w*h"
+                   {:count (count pixels)
+                    :width w
+                    :height h}))
+   :else (let [img-type (.getType img)
+               pixels (int-array pixels)]
+           (if (or (= img-type BufferedImage/TYPE_INT_ARGB)
+                   (= img-type BufferedImage/TYPE_INT_RGB))
+             (let [raster (.getRaster img)]
+               (.setDataElements raster x y w h pixels))
+             (.setRGB img x y w h pixels 0 w)))))
