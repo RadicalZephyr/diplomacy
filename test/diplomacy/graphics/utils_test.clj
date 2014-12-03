bb985c778d8097e163dbc5d30ab985a5c0262dc4
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 19:35:36 -0500

    Test all the image types!

--- test/diplomacy/graphics/utils_test.clj
+++ test/diplomacy/graphics/utils_test.clj
@@ -39,6 +39,22 @@
         (is (= img
                (set-pixels img 0 0 2 2 [1 0 1 0])))))))
 
+(defn str-type [type]
+  (case type
+    BufferedImage/TYPE_INT_RGB         "INT_RGB"
+    BufferedImage/TYPE_INT_ARGB        "INT_ARGB"
+    BufferedImage/TYPE_INT_ARGB_PRE    "INT_ARGB_PRE"
+    BufferedImage/TYPE_3BYTE_BGR       "3BYTE_BGR"
+    BufferedImage/TYPE_4BYTE_ABGR      "4BYTE_ABGR"
+    BufferedImage/TYPE_4BYTE_ABGR_PRE  "4BYTE_ABGR_PRE"
+    BufferedImage/TYPE_USHORT_555_RGB  "USHORT_555_RGB"
+    BufferedImage/TYPE_USHORT_565_RGB  "USHORT_565_RGB"
+    BufferedImage/TYPE_USHORT_GRAY     "USHORT_GRAY"
+    BufferedImage/TYPE_BYTE_BINARY     "BYTE_BINARY"
+    BufferedImage/TYPE_BYTE_GRAY       "BYTE_GRAY"
+    BufferedImage/TYPE_BYTE_INDEXED    "BYTE_INDEXED"
+    nil))
+
 (deftest round-trip-test
   (testing "Starting with a vector"
     (doseq [pixels [[1 2 3 4]
@@ -47,10 +63,18 @@
                     [1 0 1 0]]
             img-type [BufferedImage/TYPE_INT_RGB
                       BufferedImage/TYPE_INT_ARGB
+                      BufferedImage/TYPE_INT_ARGB_PRE
                       BufferedImage/TYPE_3BYTE_BGR
-                      BufferedImage/TYPE_4BYTE_ABGR]
+                      BufferedImage/TYPE_4BYTE_ABGR
+                      BufferedImage/TYPE_4BYTE_ABGR_PRE
+                      BufferedImage/TYPE_USHORT_555_RGB
+                      BufferedImage/TYPE_USHORT_565_RGB
+                      BufferedImage/TYPE_USHORT_GRAY
+                      BufferedImage/TYPE_BYTE_BINARY
+                      BufferedImage/TYPE_BYTE_GRAY
+                      BufferedImage/TYPE_BYTE_INDEXED]
             :let [img (BufferedImage. 2 2 img-type)]]
-      (testing (str pixels " and type " img-type)
+      (testing (str pixels " and type " (str-type img-type))
        (is (= pixels
               (-> img
                   (set-pixels 0 0 2 2 pixels)
