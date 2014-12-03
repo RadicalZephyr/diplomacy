ca56dd03a3b34094c2261e17515f95bb4c54dc4b
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 19:18:01 -0500

    Add some tests for set-pixels

--- test/diplomacy/graphics/utils_test.clj
+++ test/diplomacy/graphics/utils_test.clj
@@ -21,4 +21,20 @@
         (is (= (class [])
                (class (get-pixels img 0 0 10 10))))))))
 
-(deftest set-pixels-test)
+(deftest set-pixels-test
+  (testing "Nil returns"
+    (is (= (set-pixels nil 0 0 0 1 [])
+           nil))
+    (is (= (set-pixels nil 0 0 1 0 [])
+           nil))
+    (is (= (set-pixels nil 0 0 0 0 [])
+           nil)))
+
+  (testing "Set pixel of image an image from vector"
+    (doseq [img-type [BufferedImage/TYPE_INT_RGB
+                      BufferedImage/TYPE_INT_ARGB
+                      BufferedImage/TYPE_3BYTE_BGR
+                      BufferedImage/TYPE_4BYTE_ABGR]]
+      (let [img (BufferedImage. 10 10 img-type)]
+        (is (= img
+               (set-pixels img 0 0 2 2 [1 0 1 0])))))))
