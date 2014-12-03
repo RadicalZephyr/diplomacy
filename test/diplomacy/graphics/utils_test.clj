bde5d2f8f1d5acc882c25bed9d766ef8e9fe2207
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 19:13:33 -0500

    Test the return of successful gets

--- test/diplomacy/graphics/utils_test.clj
+++ test/diplomacy/graphics/utils_test.clj
@@ -1,6 +1,7 @@
 (ns diplomacy.graphics.utils-test
   (:require [clojure.test             :refer :all]
-            [diplomacy.graphics.utils :refer :all]))
+            [diplomacy.graphics.utils :refer :all])
+  (:import java.awt.image.BufferedImage))
 
 (deftest get-pixels-test
   (testing "Nil returns"
@@ -9,6 +10,15 @@
     (is (= (get-pixels nil 0 0 1 0)
            []))
     (is (= (get-pixels nil 0 0 0 0)
-           []))))
+           [])))
+
+  (testing "Get pixel vectors from an image"
+    (doseq [img-type [BufferedImage/TYPE_INT_RGB
+                      BufferedImage/TYPE_INT_ARGB
+                      BufferedImage/TYPE_3BYTE_BGR
+                      BufferedImage/TYPE_4BYTE_ABGR]]
+      (let [img (BufferedImage. 10 10 img-type)]
+        (is (= (class [])
+               (class (get-pixels img 0 0 10 10))))))))
 
 (deftest set-pixels-test)
