4f7030809b83252af8eaa30e10358a7642079706
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 19:54:37 -0500

    Get labeling working correctly

--- test/diplomacy/graphics/utils_test.clj
+++ test/diplomacy/graphics/utils_test.clj
@@ -39,21 +39,19 @@
         (is (= img
                (set-pixels img 0 0 2 2 [1 0 1 0])))))))
 
-(defn str-type [type]
-  (case type
-    BufferedImage/TYPE_INT_RGB         "INT_RGB"
-    BufferedImage/TYPE_INT_ARGB        "INT_ARGB"
-    BufferedImage/TYPE_INT_ARGB_PRE    "INT_ARGB_PRE"
-    BufferedImage/TYPE_3BYTE_BGR       "3BYTE_BGR"
-    BufferedImage/TYPE_4BYTE_ABGR      "4BYTE_ABGR"
-    BufferedImage/TYPE_4BYTE_ABGR_PRE  "4BYTE_ABGR_PRE"
-    BufferedImage/TYPE_USHORT_555_RGB  "USHORT_555_RGB"
-    BufferedImage/TYPE_USHORT_565_RGB  "USHORT_565_RGB"
-    BufferedImage/TYPE_USHORT_GRAY     "USHORT_GRAY"
-    BufferedImage/TYPE_BYTE_BINARY     "BYTE_BINARY"
-    BufferedImage/TYPE_BYTE_GRAY       "BYTE_GRAY"
-    BufferedImage/TYPE_BYTE_INDEXED    "BYTE_INDEXED"
-    nil))
+(def str-type
+  {BufferedImage/TYPE_INT_RGB        "INT_RGB"
+   BufferedImage/TYPE_INT_ARGB       "INT_ARGB"
+   BufferedImage/TYPE_INT_ARGB_PRE   "INT_ARGB_PRE"
+   BufferedImage/TYPE_3BYTE_BGR      "3BYTE_BGR"
+   BufferedImage/TYPE_4BYTE_ABGR     "4BYTE_ABGR"
+   BufferedImage/TYPE_4BYTE_ABGR_PRE "4BYTE_ABGR_PRE"
+   BufferedImage/TYPE_USHORT_565_RGB "USHORT_565_RGB"
+   BufferedImage/TYPE_USHORT_555_RGB "USHORT_555_RGB"
+   BufferedImage/TYPE_BYTE_GRAY      "BYTE_GRAY"
+   BufferedImage/TYPE_USHORT_GRAY    "USHORT_GRAY"
+   BufferedImage/TYPE_BYTE_BINARY    "BYTE_BINARY"
+   BufferedImage/TYPE_BYTE_INDEXED   "BYTE_INDEXED"})
 
 (deftest round-trip-test
   (testing "Starting with a vector"
@@ -67,12 +65,12 @@
                       BufferedImage/TYPE_3BYTE_BGR
                       BufferedImage/TYPE_4BYTE_ABGR
                       BufferedImage/TYPE_4BYTE_ABGR_PRE
-                      BufferedImage/TYPE_USHORT_555_RGB
                       BufferedImage/TYPE_USHORT_565_RGB
+                      BufferedImage/TYPE_USHORT_555_RGB
+                      BufferedImage/TYPE_BYTE_GRAY
                       BufferedImage/TYPE_USHORT_GRAY
                       BufferedImage/TYPE_BYTE_BINARY
-                      BufferedImage/TYPE_BYTE_GRAY
-                      BufferedImage/TYPE_BYTE_INDEXED]
+                      BufferedImage/TYPE_BYTE_INDEXED   ]
             :let [img (BufferedImage. 2 2 img-type)]]
       (testing (str pixels " and type " (str-type img-type))
        (is (= pixels
